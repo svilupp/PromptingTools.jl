f41c74cdafb309b283d8ed89eade4e428e55e51b
J S <49557684+svilupp@users.noreply.github.com>
2023-11-16 21:44:08 +0000

    add extraction

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,13 @@ using Aqua
 const PT = PromptingTools
 
 @testset "Code quality (Aqua.jl)" begin
-    Aqua.test_all(PromptingTools)
+    # Skipping unbound_args check because we need our `MaybeExtract` type to be unboard
+    Aqua.test_all(PromptingTools; unbound_args = false)
 end
 @testset "PromptingTools.jl" begin
     include("utils.jl")
     include("messages.jl")
+    include("extraction.jl")
     include("llm_openai.jl")
     include("templates.jl")
 end
