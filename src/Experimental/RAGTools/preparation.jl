5035662d8438031fc8ded5fd7be782b19caedbef
J S <49557684+svilupp@users.noreply.github.com>
2024-01-25 20:06:43 +0000

    OpenAI model changes

--- src/Experimental/RAGTools/preparation.jl
+++ src/Experimental/RAGTools/preparation.jl
@@ -41,7 +41,7 @@ function _normalize end
     get_chunks(files_or_docs::Vector{<:AbstractString}; reader::Symbol = :files,
         sources::Vector{<:AbstractString} = files_or_docs,
         verbose::Bool = true,
-        separators = ["\n\n", ". ", "\n"], max_length::Int = 256)
+        separators = ["\\n\\n", ". ", "\\n"], max_length::Int = 256)
 
 Chunks the provided `files_or_docs` into chunks of maximum length `max_length` (if possible with provided `separators`).
 
@@ -52,7 +52,7 @@ Supports two modes of operation:
 # Arguments
 - `files_or_docs`: A vector of valid file paths OR string documents to be chunked.
 - `reader`: A symbol indicating the type of input, can be either `:files` or `:docs`. Default is `:files`.
-- `separators`: A list of strings used as separators for splitting the text in each file into chunks. Default is `[\n\n", ". ", "\n"]`.
+- `separators`: A list of strings used as separators for splitting the text in each file into chunks. Default is `[\\n\\n", ". ", "\\n"]`.
 - `max_length`: The maximum length of each chunk (if possible with provided separators). Default is 256.
 - `sources`: A vector of strings indicating the source of each chunk. Default is equal to `files_or_docs` (for `reader=:files`)
 
