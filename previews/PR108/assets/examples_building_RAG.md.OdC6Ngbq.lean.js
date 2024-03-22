8f36dd81e924f39af064b0d8fbb0b81c17be98e3
Documenter.jl <documenter@juliadocs.github.io>
2024-03-22 18:30:59 +0000

    build based on 2e0a9ae

---
+++ previews/PR108/assets/examples_building_RAG.md.OdC6Ngbq.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.C1DzC6vG.js";
+const __pageData = JSON.parse('{"title":"Building a Simple Retrieval-Augmented Generation (RAG) System with RAGTools","description":"","frontmatter":{},"headers":[],"relativePath":"examples/building_RAG.md","filePath":"examples/building_RAG.md","lastUpdated":null}');
+const _sfc_main = { name: "examples/building_RAG.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 52);
+const _hoisted_53 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_53);
+}
+const building_RAG = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  building_RAG as default
+};
