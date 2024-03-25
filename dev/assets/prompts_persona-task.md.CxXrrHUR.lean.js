fd05de68d4e3fea3cc4ec2417226869118778d48
Documenter.jl <documenter@juliadocs.github.io>
2024-03-25 20:47:01 +0000

    build based on 7cab975

--- dev/assets/prompts_persona-task.md.CxXrrHUR.lean.js
+++ dev/assets/prompts_persona-task.md.CxXrrHUR.lean.js
@@ -67,7 +67,27 @@ const _hoisted_74 = /* @__PURE__ */ createBaseVNode("li", null, [
 const _hoisted_75 = /* @__PURE__ */ createBaseVNode("li", null, [
   /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.1")
 ], -1);
-const _hoisted_76 = /* @__PURE__ */ createStaticVNode("", 22);
+const _hoisted_76 = /* @__PURE__ */ createStaticVNode("", 36);
+const _hoisted_112 = /* @__PURE__ */ createBaseVNode("code", null, "code", -1);
+const _hoisted_113 = /* @__PURE__ */ createBaseVNode("code", null, 'instructions="None."', -1);
+const _hoisted_114 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Placeholders: "),
+    /* @__PURE__ */ createBaseVNode("code", null, "code"),
+    /* @__PURE__ */ createTextVNode(", "),
+    /* @__PURE__ */ createBaseVNode("code", null, "instructions")
+  ])
+], -1);
+const _hoisted_115 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Word count: 1643")
+], -1);
+const _hoisted_116 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Source:")
+], -1);
+const _hoisted_117 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.0")
+], -1);
+const _hoisted_118 = /* @__PURE__ */ createStaticVNode("", 4);
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [
     _hoisted_1,
@@ -125,7 +145,23 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
       _hoisted_74,
       _hoisted_75
     ]),
-    _hoisted_76
+    _hoisted_76,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: For writing Julia-style unit tests. The prompt is XML-formatted - useful for Anthropic models. It expects "),
+          _hoisted_112,
+          createTextVNode(" provided as a string (it can be the whole source code of your app). Instructions are a good way to guide the model which functions to test and how. If you don't need the instructions, set "),
+          _hoisted_113,
+          createTextVNode(". Placeholders: " + toDisplayString(_ctx.code) + ", " + toDisplayString(_ctx.instructions), 1)
+        ])
+      ]),
+      _hoisted_114,
+      _hoisted_115,
+      _hoisted_116,
+      _hoisted_117
+    ]),
+    _hoisted_118
   ]);
 }
 const personaTask = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
