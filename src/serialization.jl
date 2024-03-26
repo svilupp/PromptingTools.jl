28cf77ae2d007a3de504928a95c1a3af51ab2e53
J S <49557684+svilupp@users.noreply.github.com>
2024-03-26 10:18:52 +0000

    Add ShareGPT template (#113)

--- src/serialization.jl
+++ src/serialization.jl
@@ -60,3 +60,53 @@ Loads a conversation (`messages`) from `io_or_file`
 function load_conversation(io_or_file::Union{IO, AbstractString})
     messages = JSON3.read(io_or_file, Vector{AbstractMessage})
 end
+
+"""
+    save_conversations(schema::AbstractPromptSchema, filename::AbstractString,
+        conversations::Vector{<:AbstractVector{<:PT.AbstractMessage}})
+
+Saves provided conversations (vector of vectors of `messages`) to `filename` rendered in the particular `schema`. 
+
+Commonly used for finetuning models with `schema = ShareGPTSchema()`
+
+The format is JSON Lines, where each line is a JSON object representing one provided conversation.
+
+See also: `save_conversation`
+
+# Examples
+
+You must always provide a VECTOR of conversations
+```julia
+messages = AbstractMessage[SystemMessage("System message 1"),
+    UserMessage("User message"),
+    AIMessage("AI message")]
+conversation = [messages] # vector of vectors
+
+dir = tempdir()
+fn = joinpath(dir, "conversations.jsonl")
+save_conversations(fn, conversation)
+
+# Content of the file (one line for each conversation)
+# {"conversations":[{"value":"System message 1","from":"system"},{"value":"User message","from":"human"},{"value":"AI message","from":"gpt"}]}
+```
+"""
+function save_conversations(schema::AbstractPromptSchema, filename::AbstractString,
+        conversations::Vector{<:AbstractVector{<:AbstractMessage}})
+    @assert endswith(filename, ".jsonl") "Filename must end with `.jsonl` (JSON Lines format)."
+    io = IOBuffer()
+    for i in eachindex(conversations)
+        conv = conversations[i]
+        rendered_conv = render(schema, conv)
+        JSON3.write(io, rendered_conv)
+        # separate each conversation by newline
+        i < length(conversations) && print(io, "\n")
+    end
+    write(filename, String(take!(io)))
+    return nothing
+end
+
+# shortcut for ShareGPTSchema
+function save_conversations(filename::AbstractString,
+        conversations::Vector{<:AbstractVector{<:AbstractMessage}})
+    save_conversations(ShareGPTSchema(), filename, conversations)
+end
