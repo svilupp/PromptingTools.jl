358620c4c3e7fcd46c7cde026530003346f5e1df
Documenter.jl <documenter@juliadocs.github.io>
2024-04-15 07:27:21 +0000

    build based on 1761405

---
+++ previews/PR133/assets/frequently_asked_questions.md.0zeM3qP6.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.C3dBnaoB.js";
+const __pageData = JSON.parse('{"title":"Frequently Asked Questions","description":"","frontmatter":{},"headers":[],"relativePath":"frequently_asked_questions.md","filePath":"frequently_asked_questions.md","lastUpdated":null}');
+const _sfc_main = { name: "frequently_asked_questions.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 145);
+const _hoisted_146 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_146);
+}
+const frequently_asked_questions = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  frequently_asked_questions as default
+};
---
+++ previews/PR133/assets/prompts_visual.md.CuTvQgAe.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.C3dBnaoB.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/visual.md","filePath":"prompts/visual.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/visual.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<p>The following file is auto-generated from the <code>templates</code> folder. For any changes, please modify the source files in the <code>templates</code> folder.</p><p>To use these templates in <code>aigenerate</code>, simply provide the template name as a symbol, eg, <code>aigenerate(:MyTemplate; placeholder1 = value1)</code></p><h2 id="Visual-Templates" tabindex="-1">Visual Templates <a class="header-anchor" href="#Visual-Templates" aria-label="Permalink to &quot;Visual Templates {#Visual-Templates}&quot;">​</a></h2><h3 id="Template:-OCRTask" tabindex="-1">Template: OCRTask <a class="header-anchor" href="#Template:-OCRTask" aria-label="Permalink to &quot;Template: OCRTask {#Template:-OCRTask}&quot;">​</a></h3><ul><li><p>Description: Transcribe screenshot, scanned pages, photos, etc. Placeholders: <code>task</code></p></li><li><p>Placeholders: <code>task</code></p></li><li><p>Word count: 239</p></li><li><p>Source:</p></li><li><p>Version: 1</p></li></ul><p><strong>System Prompt:</strong></p><div class="language-plaintext vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">plaintext</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>You are a world-class OCR engine. Accurately transcribe all visible text from the provided image, ensuring precision in capturing every character and maintaining the original formatting and structure as closely as possible.</span></span></code></pre></div><p><strong>User Prompt:</strong></p><div class="language-plaintext vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">plaintext</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span># Task</span></span>\n<span class="line"><span></span></span>\n<span class="line"><span>{{task}}</span></span></code></pre></div>', 9);
+const _hoisted_10 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_10);
+}
+const visual = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  visual as default
+};
