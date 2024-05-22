7852e3786fb2cc400d59da7dbbe65fc90eede0c0
Documenter.jl <documenter@juliadocs.github.io>
2024-05-22 08:12:52 +0000

    build based on 3f3ec43

---
+++ previews/PR156/assets/prompts_extraction.md.D-YAJK2t.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.BS7mYIs4.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/extraction.md","filePath":"prompts/extraction.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/extraction.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 22);
+const _hoisted_23 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_23);
+}
+const extraction = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  extraction as default
+};
