2de07fbfb9ca9d7b66a98b9989cb1fef8777d5cb
Documenter.jl <documenter@juliadocs.github.io>
2024-06-14 17:54:10 +0000

    build based on a225323

---
+++ previews/PR163/assets/index.md.BoT-HUud.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.DpoUd5AS.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"PromptingTools.jl","tagline":"Streamline Your Interactions with GenAI Models","description":"Discover the power of GenerativeAI and build mini workflows to save you 20 minutes every day.","image":{"src":"https://img.icons8.com/dusk/64/swiss-army-knife--v1.png","alt":"Swiss Army Knife"},"actions":[{"theme":"brand","text":"Get Started","link":"/getting_started"},{"theme":"alt","text":"How It Works","link":"/how_it_works"},{"theme":"alt","text":"F.A.Q.","link":"/frequently_asked_questions"},{"theme":"alt","text":"View on GitHub","link":"https://github.com/svilupp/PromptingTools.jl"}]},"features":[{"icon":"<img width=\\"64\\" height=\\"64\\" src=\\"https://img.icons8.com/clouds/100/000000/brain.png\\" alt=\\"Simplify\\"/>","title":"Simplify Prompt Engineering","details":"Leverage prompt templates with placeholders to make complex prompts easy."},{"icon":"<img width=\\"60\\" height=\\"60\\" src=\\"https://img.icons8.com/papercut/60/connected.png\\" alt=\\"Integration\\"/>","title":"Effortless Integration","details":"Fire quick questions with @ai_str macro and light wrapper types. Minimal dependencies for seamless integration."},{"icon":"<img width=\\"64\\" height=\\"64\\" src=\\"https://img.icons8.com/dusk/64/search--v1.png\\" alt=\\"Discoverability\\"/>","title":"Designed for Discoverability","details":"Efficient access to cutting-edge models with intuitive ai* functions. Stay in the flow with minimal context switching."}]},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}');
+const _sfc_main = { name: "index.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 2);
+const _hoisted_3 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
---
+++ previews/PR163/assets/prompts_RAG.md.DIktphzP.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.DpoUd5AS.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/RAG.md","filePath":"prompts/RAG.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/RAG.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 79);
+const _hoisted_80 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_80);
+}
+const RAG = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  RAG as default
+};
