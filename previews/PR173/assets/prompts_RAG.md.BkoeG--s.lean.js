6105930da40e498303330779668afb62384a42fc
Documenter.jl <documenter@juliadocs.github.io>
2024-07-01 09:58:18 +0000

    build based on b2ad703

---
+++ previews/PR173/assets/prompts_RAG.md.BkoeG--s.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.BFRe7Cwt.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/RAG.md","filePath":"prompts/RAG.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/RAG.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 86);
+const _hoisted_87 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_87);
+}
+const RAG = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  RAG as default
+};
---
+++ previews/PR173/assets/prompts_persona-task.md.DdID5SwJ.lean.js
@@ -0,0 +1,171 @@
+import { _ as _export_sfc, c as createElementBlock, m as createBaseVNode, a as createTextVNode, t as toDisplayString, a7 as createStaticVNode, o as openBlock } from "./chunks/framework.BFRe7Cwt.js";
+const __pageData = JSON.parse('{"title":"Theme 1: [Theme Description]","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/persona-task.md","filePath":"prompts/persona-task.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/persona-task.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 10);
+const _hoisted_11 = /* @__PURE__ */ createBaseVNode("code", null, 'instructions="None."', -1);
+const _hoisted_12 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Placeholders: "),
+    /* @__PURE__ */ createBaseVNode("code", null, "transcript"),
+    /* @__PURE__ */ createTextVNode(", "),
+    /* @__PURE__ */ createBaseVNode("code", null, "instructions")
+  ])
+], -1);
+const _hoisted_13 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Word count: 2190")
+], -1);
+const _hoisted_14 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Source: Evolved from "),
+    /* @__PURE__ */ createBaseVNode("a", {
+      href: "https://github.com/jxnl/youtubechapters-backend/blob/main/summary_app/md_summarize.py",
+      target: "_blank",
+      rel: "noreferrer"
+    }, "jxnl's Youtube Chapters prompt")
+  ])
+], -1);
+const _hoisted_15 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.1")
+], -1);
+const _hoisted_16 = /* @__PURE__ */ createStaticVNode("", 5);
+const _hoisted_21 = /* @__PURE__ */ createBaseVNode("code", null, 'instructions="None."', -1);
+const _hoisted_22 = /* @__PURE__ */ createStaticVNode("", 4);
+const _hoisted_26 = /* @__PURE__ */ createStaticVNode("", 28);
+const _hoisted_54 = /* @__PURE__ */ createBaseVNode("code", null, "Follow up email. Sections: Agreements, Next steps", -1);
+const _hoisted_55 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Placeholders: "),
+    /* @__PURE__ */ createBaseVNode("code", null, "brief")
+  ])
+], -1);
+const _hoisted_56 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Word count: 1204")
+], -1);
+const _hoisted_57 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Source:")
+], -1);
+const _hoisted_58 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.1")
+], -1);
+const _hoisted_59 = /* @__PURE__ */ createStaticVNode("", 41);
+const _hoisted_100 = /* @__PURE__ */ createBaseVNode("code", null, "code", -1);
+const _hoisted_101 = /* @__PURE__ */ createBaseVNode("code", null, 'instructions="None."', -1);
+const _hoisted_102 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Placeholders: "),
+    /* @__PURE__ */ createBaseVNode("code", null, "code"),
+    /* @__PURE__ */ createTextVNode(", "),
+    /* @__PURE__ */ createBaseVNode("code", null, "instructions")
+  ])
+], -1);
+const _hoisted_103 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Word count: 1475")
+], -1);
+const _hoisted_104 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Source:")
+], -1);
+const _hoisted_105 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.1")
+], -1);
+const _hoisted_106 = /* @__PURE__ */ createStaticVNode("", 42);
+const _hoisted_148 = /* @__PURE__ */ createBaseVNode("code", null, "code", -1);
+const _hoisted_149 = /* @__PURE__ */ createBaseVNode("code", null, 'instructions="None."', -1);
+const _hoisted_150 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, [
+    /* @__PURE__ */ createTextVNode("Placeholders: "),
+    /* @__PURE__ */ createBaseVNode("code", null, "code"),
+    /* @__PURE__ */ createTextVNode(", "),
+    /* @__PURE__ */ createBaseVNode("code", null, "instructions")
+  ])
+], -1);
+const _hoisted_151 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Word count: 1643")
+], -1);
+const _hoisted_152 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Source:")
+], -1);
+const _hoisted_153 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Version: 1.0")
+], -1);
+const _hoisted_154 = /* @__PURE__ */ createStaticVNode("", 4);
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: Template for summarizing transcripts of videos and meetings into the decisions made and the agreed next steps. If you don't need the instructions, set "),
+          _hoisted_11,
+          createTextVNode(". Placeholders: " + toDisplayString(_ctx.transcript) + ", " + toDisplayString(_ctx.instructions), 1)
+        ])
+      ]),
+      _hoisted_12,
+      _hoisted_13,
+      _hoisted_14,
+      _hoisted_15
+    ]),
+    _hoisted_16,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: Template for summarizing survey verbatim responses into 3-5 themes with an example for each theme. If you don't need the instructions, set "),
+          _hoisted_21,
+          createTextVNode(". Placeholders: " + toDisplayString(_ctx.question) + ", " + toDisplayString(_ctx.responses) + ", " + toDisplayString(_ctx.instructions), 1)
+        ])
+      ]),
+      _hoisted_22
+    ]),
+    _hoisted_26,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: Template for quick email drafts. Provide a brief in 5-7 words as headlines, eg, "),
+          _hoisted_54,
+          createTextVNode(" Placeholders: " + toDisplayString(_ctx.brief), 1)
+        ])
+      ]),
+      _hoisted_55,
+      _hoisted_56,
+      _hoisted_57,
+      _hoisted_58
+    ]),
+    _hoisted_59,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: For writing Julia-style unit tests. It expects "),
+          _hoisted_100,
+          createTextVNode(" provided as a string (it can be the whole source code of your app). Instructions are a good way to guide the model which functions to test and how. If you don't need the instructions, set "),
+          _hoisted_101,
+          createTextVNode(". Placeholders: " + toDisplayString(_ctx.code) + ", " + toDisplayString(_ctx.instructions), 1)
+        ])
+      ]),
+      _hoisted_102,
+      _hoisted_103,
+      _hoisted_104,
+      _hoisted_105
+    ]),
+    _hoisted_106,
+    createBaseVNode("ul", null, [
+      createBaseVNode("li", null, [
+        createBaseVNode("p", null, [
+          createTextVNode("Description: For writing Julia-style unit tests. The prompt is XML-formatted - useful for Anthropic models. It expects "),
+          _hoisted_148,
+          createTextVNode(" provided as a string (it can be the whole source code of your app). Instructions are a good way to guide the model which functions to test and how. If you don't need the instructions, set "),
+          _hoisted_149,
+          createTextVNode(". Placeholders: " + toDisplayString(_ctx.code) + ", " + toDisplayString(_ctx.instructions), 1)
+        ])
+      ]),
+      _hoisted_150,
+      _hoisted_151,
+      _hoisted_152,
+      _hoisted_153
+    ]),
+    _hoisted_154
+  ]);
+}
+const personaTask = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  personaTask as default
+};
