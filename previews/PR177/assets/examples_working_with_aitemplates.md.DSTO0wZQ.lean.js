b5213783d1f666e40d1b748ebec721a6bfc5f341
Documenter.jl <documenter@juliadocs.github.io>
2024-07-13 10:56:18 +0000

    build based on 25265c2

---
+++ previews/PR177/assets/examples_working_with_aitemplates.md.DSTO0wZQ.lean.js
@@ -0,0 +1,17 @@
+import { _ as _export_sfc, c as createElementBlock, m as createBaseVNode, t as toDisplayString, a7 as createStaticVNode, o as openBlock } from "./chunks/framework.BPyFnm0U.js";
+const __pageData = JSON.parse('{"title":"Using AITemplates","description":"","frontmatter":{},"headers":[],"relativePath":"examples/working_with_aitemplates.md","filePath":"examples/working_with_aitemplates.md","lastUpdated":null}');
+const _sfc_main = { name: "examples/working_with_aitemplates.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 6);
+const _hoisted_7 = /* @__PURE__ */ createStaticVNode("", 28);
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    createBaseVNode("p", null, 'AITemplates are just a collection of templated prompts (ie, set of "messages" that have placeholders like ' + toDisplayString(_ctx.question) + ")", 1),
+    _hoisted_7
+  ]);
+}
+const working_with_aitemplates = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  working_with_aitemplates as default
+};
---
+++ previews/PR177/assets/prompts_agents.md.7RFcVDnR.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a7 as createStaticVNode } from "./chunks/framework.BPyFnm0U.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/agents.md","filePath":"prompts/agents.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/agents.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 28);
+const _hoisted_29 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_29);
+}
+const agents = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  agents as default
+};
