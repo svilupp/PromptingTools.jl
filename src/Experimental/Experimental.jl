6ef4dda4ba095a4ff27ed8db4f7ac0d1aa19ccc1
Pablo Valdunciel <pablo.valdunciel@docyet.com>
2025-01-21 10:14:32 +0100

    chore: delete code files

--- src/Experimental/Experimental.jl
+++ src/Experimental/Experimental.jl
@@ -5,7 +5,6 @@ This module is for experimental code that is not yet ready for production.
 It is not included in the main module, so it must be explicitly imported.
 
 Contains:
-- `RAGTools`: Retrieval-Augmented Generation (RAG) functionality.
 - `AgentTools`: Agentic functionality - lazy calls for building pipelines (eg, `AIGenerate`) and `AICodeFixer`.
 - `APITools`: APIs to complement GenAI workflows (eg, Tavily Search API).
 """
@@ -13,9 +12,6 @@ module Experimental
 
 export APITools
 include("APITools/APITools.jl")
-
-export RAGTools
-include("RAGTools/RAGTools.jl")
 
 export AgentTools
 include("AgentTools/AgentTools.jl")
