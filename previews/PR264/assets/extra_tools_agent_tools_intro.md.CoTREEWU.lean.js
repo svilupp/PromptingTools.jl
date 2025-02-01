690f31a5640902dd347443e7409d09d3fba44fa2
Documenter.jl <documenter@juliadocs.github.io>
2025-02-01 16:01:50 +0000

    build based on 0c185a7

---
+++ previews/PR264/assets/extra_tools_agent_tools_intro.md.CoTREEWU.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.BMoe2FdN.js";
+const __pageData = JSON.parse('{"title":"Agent Tools Introduction","description":"","frontmatter":{},"headers":[],"relativePath":"extra_tools/agent_tools_intro.md","filePath":"extra_tools/agent_tools_intro.md","lastUpdated":null}');
+const _sfc_main = { name: "extra_tools/agent_tools_intro.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _cache[0] || (_cache[0] = [
+    createStaticVNode("", 42)
+  ]));
+}
+const agent_tools_intro = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  agent_tools_intro as default
+};
---
+++ previews/PR264/assets/extra_tools_rag_tools_intro.md.BYIcvj4H.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.BMoe2FdN.js";
+const _imports_0 = "/PromptingTools.jl/previews/PR264/assets/rag_diagram_highlevel.D_aLugML.png";
+const _imports_1 = "/PromptingTools.jl/previews/PR264/assets/rag_diagram_detailed._BjqL9Ae.png";
+const __pageData = JSON.parse('{"title":"RAG Tools Introduction","description":"","frontmatter":{},"headers":[],"relativePath":"extra_tools/rag_tools_intro.md","filePath":"extra_tools/rag_tools_intro.md","lastUpdated":null}');
+const _sfc_main = { name: "extra_tools/rag_tools_intro.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _cache[0] || (_cache[0] = [
+    createStaticVNode("", 88)
+  ]));
+}
+const rag_tools_intro = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  rag_tools_intro as default
+};
