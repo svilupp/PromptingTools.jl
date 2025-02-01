1475d9e3880a44e1d3581bcdfd7cd26b98009d72
Documenter.jl <documenter@juliadocs.github.io>
2025-02-01 16:52:44 +0000

    build based on 98d051c

---
+++ previews/PR265/assets/examples_readme_examples.md.BZjRTUpf.lean.js
@@ -0,0 +1,21 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode, j as createBaseVNode, a as createTextVNode, t as toDisplayString } from "./chunks/framework.DYLd_DxG.js";
+const __pageData = JSON.parse('{"title":"Various Examples","description":"","frontmatter":{},"headers":[],"relativePath":"examples/readme_examples.md","filePath":"examples/readme_examples.md","lastUpdated":null}');
+const _sfc_main = { name: "examples/readme_examples.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [
+    _cache[4] || (_cache[4] = createStaticVNode("", 28)),
+    createBaseVNode("p", null, [
+      _cache[0] || (_cache[0] = createTextVNode("You can use the ")),
+      _cache[1] || (_cache[1] = createBaseVNode("code", null, "aigenerate", -1)),
+      _cache[2] || (_cache[2] = createTextVNode(" function to replace handlebar variables (eg, ")),
+      createBaseVNode("code", null, toDisplayString(_ctx.name), 1),
+      _cache[3] || (_cache[3] = createTextVNode(") via keyword arguments."))
+    ]),
+    _cache[5] || (_cache[5] = createStaticVNode("", 104))
+  ]);
+}
+const readme_examples = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  readme_examples as default
+};
