1d09ffe9c6002efa04404f316089fe0412f65883
Documenter.jl <documenter@juliadocs.github.io>
2025-05-07 09:36:11 +0000

    build based on c1d15c8

--- dev/assets/reference.md.DuoT4fYR.lean.js
+++ dev/assets/reference.md.DuoT4fYR.lean.js
@@ -28,7 +28,7 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
       ]),
       _cache[5] || (_cache[5] = createBaseVNode("p", null, [
         createBaseVNode("a", {
-          href: "https://github.com/svilupp/PromptingTools.jl/blob/06cf9c017f3ff7a937c98582fe5ecd87f870f42b/src/llm_interface.jl#L392-L406",
+          href: "https://github.com/svilupp/PromptingTools.jl/blob/c1d15c8acb60f88f3603bf7ac95148015f5b58d3/src/llm_interface.jl#L392-L406",
           target: "_blank",
           rel: "noreferrer"
         }, "source")
@@ -104,7 +104,7 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
       ], -1)),
       _cache[32] || (_cache[32] = createBaseVNode("p", null, [
         createBaseVNode("a", {
-          href: "https://github.com/svilupp/PromptingTools.jl/blob/06cf9c017f3ff7a937c98582fe5ecd87f870f42b/src/llm_ollama_managed.jl#L9-L21",
+          href: "https://github.com/svilupp/PromptingTools.jl/blob/c1d15c8acb60f88f3603bf7ac95148015f5b58d3/src/llm_ollama_managed.jl#L9-L21",
           target: "_blank",
           rel: "noreferrer"
         }, "source")
@@ -150,7 +150,7 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
       ]),
       _cache[46] || (_cache[46] = createBaseVNode("p", null, [
         createBaseVNode("a", {
-          href: "https://github.com/svilupp/PromptingTools.jl/blob/06cf9c017f3ff7a937c98582fe5ecd87f870f42b/src/llm_shared.jl#L12-L32",
+          href: "https://github.com/svilupp/PromptingTools.jl/blob/c1d15c8acb60f88f3603bf7ac95148015f5b58d3/src/llm_shared.jl#L12-L32",
           target: "_blank",
           rel: "noreferrer"
         }, "source")
@@ -533,7 +533,7 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
       createBaseVNode("p", null, 'tool_map = tool_call_signature(MyMeasurement; hidden_fields = ["ctx_user_id"]) ```'),
       createBaseVNode("p", null, [
         createBaseVNode("a", {
-          href: "https://github.com/svilupp/PromptingTools.jl/blob/06cf9c017f3ff7a937c98582fe5ecd87f870f42b/src/extraction.jl#L463-L555",
+          href: "https://github.com/svilupp/PromptingTools.jl/blob/c1d15c8acb60f88f3603bf7ac95148015f5b58d3/src/extraction.jl#L463-L555",
           target: "_blank",
           rel: "noreferrer"
         }, "source")
