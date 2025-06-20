d533b1df4b0c44fc32b2c5367c79a0e2c7e30ac7
Documenter.jl <documenter@juliadocs.github.io>
2025-06-20 16:22:21 +0000

    build based on a74c4f8

---
+++ dev/assets/reference_experimental.md.BCXUjKIG.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.Dzh0eEJd.js";
+const __pageData = JSON.parse('{"title":"Reference for Experimental Module","description":"","frontmatter":{},"headers":[],"relativePath":"reference_experimental.md","filePath":"reference_experimental.md","lastUpdated":null}');
+const _sfc_main = { name: "reference_experimental.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _cache[0] || (_cache[0] = [
+    createStaticVNode("", 5)
+  ]));
+}
+const reference_experimental = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  reference_experimental as default
+};
