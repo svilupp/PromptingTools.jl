9c0e44195b437dd5fda73da1b73bcec6c03adf7d
Documenter.jl <documenter@juliadocs.github.io>
2025-07-14 09:42:45 +0000

    build based on 2320886

--- previews/PR301/assets/app.C6c0inqM.js
+++ previews/PR301/assets/app.C6c0inqM.js
@@ -1,4 +1,4 @@
-import { R as RawTheme } from "./chunks/theme.CrhbKBI1.js";
+import { R as RawTheme } from "./chunks/theme.BZfgSCZZ.js";
 import { R as inBrowser, a5 as useUpdateHead, a6 as RouterSymbol, a7 as initData, a8 as dataSymbol, a9 as Content, aa as ClientOnly, ab as siteDataRef, ac as createRouter, ad as pathToFile, ae as createSSRApp, d as defineComponent, u as useData, v as onMounted, s as watchEffect, af as usePrefetch, ag as useCopyCode, ah as useCodeGroups, a4 as h } from "./chunks/framework.DBk4e0I6.js";
 function resolveThemeExtends(theme) {
   if (theme.extends) {
