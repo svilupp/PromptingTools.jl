93c4726e3fc5955065ebfc293b84c141a9474de7
Documenter.jl <documenter@juliadocs.github.io>
2025-08-08 11:04:14 +0000

    build based on 7dfc3d7

--- previews/PR304/assets/app.wfLvz34j.js
+++ previews/PR304/assets/app.wfLvz34j.js
@@ -1,4 +1,4 @@
-import { R as RawTheme } from "./chunks/theme.wpK27nBH.js";
+import { R as RawTheme } from "./chunks/theme.5QapMb7p.js";
 import { R as inBrowser, a5 as useUpdateHead, a6 as RouterSymbol, a7 as initData, a8 as dataSymbol, a9 as Content, aa as ClientOnly, ab as siteDataRef, ac as createRouter, ad as pathToFile, ae as createSSRApp, d as defineComponent, u as useData, v as onMounted, s as watchEffect, af as usePrefetch, ag as useCopyCode, ah as useCodeGroups, a4 as h } from "./chunks/framework.hJzYR6j4.js";
 function resolveThemeExtends(theme) {
   if (theme.extends) {
