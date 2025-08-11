a7cd024bbe9b6c3bd23cba9ee88bbacd5018e064
SixZero <havliktomi@hotmail.com>
2025-08-11 14:08:44 +0200

    Unnecessary assumptions removed.

--- src/streaming.jl
+++ src/streaming.jl
@@ -10,9 +10,3 @@ end
 function streamed_request!(cb::AbstractStreamCallback, url, headers, input; kwargs...)
     error("Unimplemented streamed_request! for $(typeof(cb)).")
 end
-
-# this is from StreamCallbacks.jl
-Base.empty!(cb::AbstractStreamCallback) = empty!(cb.chunks)
-Base.push!(cb::AbstractStreamCallback, chunk::StreamChunk) = push!(cb.chunks, chunk)
-Base.isempty(cb::AbstractStreamCallback) = isempty(cb.chunks)
-Base.length(cb::AbstractStreamCallback) = length(cb.chunks)
