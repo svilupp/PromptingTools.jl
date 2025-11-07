1305b1e3983cfc5e01c5583c72aa59d775533b77
Documenter.jl <documenter@juliadocs.github.io>
2025-11-07 10:18:28 +0000

    build based on 83d95cd

---
+++ previews/PR315/assets/getting_started.md.sM3kBn-m.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.Bhu6esiU.js";
+const __pageData = JSON.parse('{"title":"Getting Started","description":"","frontmatter":{},"headers":[],"relativePath":"getting_started.md","filePath":"getting_started.md","lastUpdated":null}');
+const _sfc_main = { name: "getting_started.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 37)
+  ])]);
+}
+const getting_started = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  getting_started as default
+};
