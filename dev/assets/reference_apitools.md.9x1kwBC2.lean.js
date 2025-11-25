97e25e929837ceb0ffca312c349d582843256bd4
Documenter.jl <documenter@juliadocs.github.io>
2025-11-25 08:49:55 +0000

    build based on 2597feb

---
+++ dev/assets/reference_apitools.md.9x1kwBC2.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.CYjWD-_M.js";
+const __pageData = JSON.parse('{"title":"Reference for APITools","description":"","frontmatter":{},"headers":[],"relativePath":"reference_apitools.md","filePath":"reference_apitools.md","lastUpdated":null}');
+const _sfc_main = { name: "reference_apitools.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 6)
+  ])]);
+}
+const reference_apitools = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  reference_apitools as default
+};
