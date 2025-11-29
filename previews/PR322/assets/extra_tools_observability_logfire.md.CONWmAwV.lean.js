8cdcee6898798e258b977afe4b6425dc3accb6d9
Documenter.jl <documenter@juliadocs.github.io>
2025-11-29 01:02:02 +0000

    build based on 0109384

---
+++ previews/PR322/assets/extra_tools_observability_logfire.md.CONWmAwV.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.DEs4tLG6.js";
+const __pageData = JSON.parse('{"title":"Observability with Logfire.jl","description":"","frontmatter":{},"headers":[],"relativePath":"extra_tools/observability_logfire.md","filePath":"extra_tools/observability_logfire.md","lastUpdated":null}');
+const _sfc_main = { name: "extra_tools/observability_logfire.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 49)
+  ])]);
+}
+const observability_logfire = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  observability_logfire as default
+};
