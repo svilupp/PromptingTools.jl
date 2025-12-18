e5198ff7942f9f654d67df3f59e3bcaceed8ad27
Documenter.jl <documenter@juliadocs.github.io>
2025-12-18 18:54:04 +0000

    build based on 9c46589

---
+++ dev/assets/prompts_visual.md.uiUjWxG7.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, ai as createStaticVNode } from "./chunks/framework.CCrdOll_.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"prompts/visual.md","filePath":"prompts/visual.md","lastUpdated":null}');
+const _sfc_main = { name: "prompts/visual.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 15)
+  ])]);
+}
+const visual = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  visual as default
+};
